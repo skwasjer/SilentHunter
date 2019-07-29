6b3f50e5779ad76aecb14ab18e0fe0b745d9cf31
skwas <skwas@skwas.net>
2019-07-30 00:31:19 +0200

    Use lazy to read help text

--- src/SilentHunter.Formats/Dat/Controllers/ControllerAssemblyHelpText.cs
+++ src/SilentHunter.Formats/Dat/Controllers/ControllerAssemblyHelpText.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using System.Xml;
 using SilentHunter.Formats;
 
@@ -10,66 +11,61 @@ namespace SilentHunter.Dat.Controllers
 {
 	public class ControllerAssemblyHelpText
 	{
-		private IDictionary<string, string> _dict = new Dictionary<string, string>(); 
+		private Lazy<IDictionary<string, string>> _loadHelpText;
 
-		/// <summary>
-		/// Loads help text into a dictionary.
-		/// </summary>
-		/// <param name="docFile">The doc file to load from.</param>
-		public void Load(string docFile)
+		public ControllerAssemblyHelpText(string docFile)
+			: this(XmlReader.Create(docFile), true)
 		{
-			using (var xmlReader = XmlReader.Create(docFile))
-				Load(xmlReader);
 		}
 
-		/// <summary>
-		/// Loads help text into a dictionary.
-		/// </summary>
-		/// <param name="stream">The stream to load the doc file from.</param>
-		public void Load(Stream stream)
+		public ControllerAssemblyHelpText(Stream stream, bool disposeReader = false)
+			: this(XmlReader.Create(stream), disposeReader)
 		{
-			using (var xmlReader = XmlReader.Create(stream))
-				Load(xmlReader);
 		}
 
-		/// <summary>
-		/// Loads help text into a dictionary.
-		/// </summary>
-		/// <param name="xmlReader">The reader to load the doc file from.</param>
-		public void Load(XmlReader xmlReader)
+		public ControllerAssemblyHelpText(XmlReader xmlReader, bool disposeReader = false)
 		{
-			_dict.Clear();
-			
+			_loadHelpText = new Lazy<IDictionary<string, string>>(() => LoadHelpText(xmlReader, disposeReader), LazyThreadSafetyMode.ExecutionAndPublication);
+		}
+
+		private static IDictionary<string, string> LoadHelpText(XmlReader xmlReader, bool disposeReader)
+		{
+			var dict = new Dictionary<string, string>();
 			do
 			{
-				if (!xmlReader.ReadToFollowing("member")) break;
-				var name = xmlReader.GetAttribute("name");
-				if (string.IsNullOrEmpty(name) || name.StartsWith("M:")) continue;
-				if (!xmlReader.ReadToDescendant("summary")) break;
-				_dict.Add(name, xmlReader.ReadInnerXml().Trim());
+				if (!xmlReader.ReadToFollowing("member"))
+				{
+					break;
+				}
+
+				string name = xmlReader.GetAttribute("name");
+				if (string.IsNullOrEmpty(name) || name.StartsWith("M:"))
+				{
+					continue;
+				}
+
+				if (!xmlReader.ReadToDescendant("summary"))
+				{
+					break;
+				}
+
+				dict.Add(name, xmlReader.ReadInnerXml().Trim());
 			} while (!xmlReader.EOF);
-		}
 
-		public string this[string key]
-		{
-			get { return _dict.ContainsKey(key) ? _dict[key] : null; }
-		}
-
-		public string this[Type type]
-		{
-			get
+			if (disposeReader)
 			{
-				return type.GetAttribute<DescriptionAttribute>()?.Description ?? this["T:" + type.FullName];
+				xmlReader.Dispose();
 			}
+
+			return dict;
 		}
 
-		public string this[MemberInfo member]
-		{
-			get
-			{
-				return member.GetAttribute<DescriptionAttribute>()?.Description 
-					?? this[string.Format("{0}:{1}.{2}", member.MemberType.ToString()[0], member.DeclaringType.FullName, member.Name)];
-			} 
-		}
+		public string this[string key] => _loadHelpText.Value.ContainsKey(key) ? _loadHelpText.Value[key] : null;
+
+		public string this[Type type] => type.GetAttribute<DescriptionAttribute>()?.Description ?? this["T:" + type.FullName];
+
+		public string this[MemberInfo member] =>
+			member.GetAttribute<DescriptionAttribute>()?.Description
+			?? this[$"{member.MemberType.ToString()[0]}:{member.DeclaringType.FullName}.{member.Name}"];
 	}
 }
