f810cc797446eebfb59c8419c189906a6e36484e
skwas <skwas@skwas.net>
2019-07-30 07:49:37 +0200

    Use Activator to create (public) instance and code style

--- src/skwas.IO/ChunkedFiles/ChunkReader.cs
+++ src/skwas.IO/ChunkedFiles/ChunkReader.cs
@@ -1,36 +1,40 @@
 using System;
-using System.Reflection;
 using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace skwas.IO
 {
 	/// <summary>
-	/// Represents a reader with which <see cref="IChunk"/>'s can be read from a stream.
+	/// Represents a reader with which <see cref="IChunk" />'s can be read from a stream.
 	/// </summary>
 	/// <typeparam name="TMagic">The type of the magic.</typeparam>
 	/// <typeparam name="TChunk">The type of the chunk.</typeparam>
 	public class ChunkReader<TMagic, TChunk> : IDisposable
 		where TChunk : IChunk
 	{
-		private bool _disposed;
 		private BinaryReader _reader;
 		private IChunkResolver _chunkResolver;
 
 		#region Implementation of IDisposable / .ctor
 
 		/// <summary>
-		/// Initializes a new instance of <see cref="ChunkReader{TMagic,TChunk}"/>.
+		/// Initializes a new instance of <see cref="ChunkReader{TMagic,TChunk}" />.
 		/// </summary>
 		/// <param name="stream">The stream to read from.</param>
 		/// <param name="chunkResolver">The resolver to use to find the type associated with the magic.</param>
 		/// <param name="leaveOpen">True to leave the stream open.</param>
 		public ChunkReader(Stream stream, IChunkResolver chunkResolver, bool leaveOpen)
-		{			
+		{
 			if (stream == null)
+			{
 				throw new ArgumentNullException(nameof(stream));
+			}
+
 			if (chunkResolver == null)
+			{
 				throw new ArgumentNullException(nameof(chunkResolver));
+			}
 
 			_reader = new BinaryReader(stream, Encoding.Default, leaveOpen);
 			_chunkResolver = chunkResolver;
@@ -50,7 +54,11 @@ namespace skwas.IO
 		/// <param name="disposing">True if disposing.</param>
 		protected virtual void Dispose(bool disposing)
 		{
-			if (_disposed) return;
+			if (IsDisposed)
+			{
+				return;
+			}
+
 			if (disposing)
 			{
 				// Dispose managed.
@@ -61,11 +69,11 @@ namespace skwas.IO
 			}
 			// Dispose unmanaged.
 
-			_disposed = true;
+			IsDisposed = true;
 		}
 
 		/// <summary>
-		/// Releases all resources associated with this <see cref="ChunkReader{TMagic,TChunk}"/> object.
+		/// Releases all resources associated with this <see cref="ChunkReader{TMagic,TChunk}" /> object.
 		/// </summary>
 		public void Dispose()
 		{
@@ -76,7 +84,7 @@ namespace skwas.IO
 		/// <summary>
 		/// Gets whether the object is disposed.
 		/// </summary>
-		protected bool IsDisposed => _disposed;
+		protected bool IsDisposed { get; private set; }
 
 		#endregion
 
@@ -84,7 +92,7 @@ namespace skwas.IO
 		/// Gets the underlying stream.
 		/// </summary>
 		public virtual Stream BaseStream => _reader.BaseStream;
-		
+
 		/// <summary>
 		/// Reads the magic from the stream.
 		/// </summary>
@@ -92,7 +100,9 @@ namespace skwas.IO
 		public virtual TMagic ReadMagic()
 		{
 			if (IsDisposed)
+			{
 				throw new ObjectDisposedException(GetType().Name);
+			}
 
 			return (TMagic)_reader.ReadStruct(typeof(TMagic));
 		}
@@ -104,35 +114,41 @@ namespace skwas.IO
 		public virtual TChunk Read()
 		{
 			if (IsDisposed)
+			{
 				throw new ObjectDisposedException(GetType().Name);
+			}
 
-			if (BaseStream.Position >= BaseStream.Length) return default(TChunk);
+			if (BaseStream.Position >= BaseStream.Length)
+			{
+				return default;
+			}
 
-			var offset = BaseStream.Position;
+			long offset = BaseStream.Position;
 			// Read magic.
-			var magic = ReadMagic();
+			TMagic magic = ReadMagic();
 
 			// If not a valuetype, check if the magic is null. This indicates that the stream is empty, and thus we have to exit.
 			if (!typeof(TMagic).IsValueType && magic == null)
-				return default(TChunk);
+			{
+				return default;
+			}
 
-			var chunkType = _chunkResolver.Resolve(magic) ?? typeof(TChunk);
+			Type chunkType = _chunkResolver.Resolve(magic) ?? typeof(TChunk);
 
 			// First check for a .ctor with single param of type TMagic.
 			TChunk newChunk;
-			var constructor = chunkType.GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, new[] { typeof(TMagic) }, null);
+			ConstructorInfo constructor = chunkType.GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, new[] { typeof(TMagic) }, null);
 			if (constructor != null)
-				newChunk = (TChunk)constructor.Invoke(new object[] { magic });
+			{
+				newChunk = (TChunk)Activator.CreateInstance(chunkType, magic);
+			}
 			else
 			{
 				// Try a parameterless constructor.
-				constructor = chunkType.GetConstructor(Type.EmptyTypes);
-				if (constructor == null)
-					throw new IOException(string.Format("The specified type '{0}' does not have a valid constructor.", chunkType.Name));
-				newChunk = (TChunk)constructor.Invoke(null);
-				newChunk.Magic = magic;
+				newChunk = (TChunk)Activator.CreateInstance(chunkType);
 			}
-			//newChunk.ParentFile = _parent;
+
+			newChunk.Magic = magic;
 			newChunk.FileOffset = offset;
 			((IChunk<TMagic>)newChunk).Deserialize(BaseStream);
 			return newChunk;
