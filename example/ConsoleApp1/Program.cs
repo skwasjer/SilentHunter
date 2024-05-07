3167ae12cd24745a2aa2fcdef8da3024277afc65
Bukowa <gitbukowa@gmail.com>
2024-05-07 18:35:44 +0200

    feat: add example project
    Signed-off-by: Bukowa <gitbukowa@gmail.com>

---
+++ example/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Particles;
+using SilentHunter.FileFormats.Dat;
+using SilentHunter.FileFormats.DependencyInjection;
+
+IServiceCollection svcCollection = new ServiceCollection();
+svcCollection.AddSilentHunterParsers(configurer => configurer.Controllers.FromAssembly(typeof(ParticleGenerator).Assembly));
+ServiceProvider svcProvider = svcCollection.BuildServiceProvider();
+DatFile datFile = svcProvider.GetRequiredService<DatFile>();
+
+const string fPath = "../../../AI_Sensors.dat";
+
+await datFile.LoadAsync(fPath);
+
+Console.WriteLine(datFile.Chunks[100].Id);
